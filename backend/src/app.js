d085b380ab19ffa6b317a17e2ef1a2fff7eb4b62
Mitesh Patil <mapatilk101@gmail.com>
2026-04-24 15:39:50 +0530

    final config

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://stock-guru-self.vercel.app"
+    "https://stock-guru-rust.vercel.app"
   ],
   credentials: true
 }));
